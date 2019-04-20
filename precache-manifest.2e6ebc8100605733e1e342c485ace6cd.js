05f2cd4cfc1fe70b2d711e9d3376d86a96ea9a01
Jongyeol Baek <geeksbaek@gmail.com>
2019-04-21 02:20:00 +0900

    Updates

--- precache-manifest.2e6ebc8100605733e1e342c485ace6cd.js
+++ precache-manifest.2e6ebc8100605733e1e342c485ace6cd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tesla-model-3-korea/static/js/runtime~main.6de470a6.js"
   },
   {
-    "revision": "5029055cfc57703bc432",
-    "url": "/tesla-model-3-korea/static/js/main.83f05672.chunk.js"
+    "revision": "e8968ac3ff62c9f81ee4",
+    "url": "/tesla-model-3-korea/static/js/main.4cf14b9b.chunk.js"
   },
   {
     "revision": "166f6bd214788e15618b",
     "url": "/tesla-model-3-korea/static/js/2.4fb18c60.chunk.js"
   },
   {
-    "revision": "5029055cfc57703bc432",
+    "revision": "e8968ac3ff62c9f81ee4",
     "url": "/tesla-model-3-korea/static/css/main.c54a3513.chunk.css"
   },
   {
-    "revision": "cbe514799c3dd2e15de666014bcacffa",
+    "revision": "5624cb4c2b98ac0bc69cdf13f73aacb9",
     "url": "/tesla-model-3-korea/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tesla-model-3-korea/precache-manifest.e220da545bed67349338a3bbaeef0775.js"
+  "/tesla-model-3-korea/precache-manifest.2e6ebc8100605733e1e342c485ace6cd.js"
 );
 
 workbox.clientsClaim();
